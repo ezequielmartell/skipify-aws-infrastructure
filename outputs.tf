593c158d707742e7a3485d52b5af1483719b2b8c
ezequielmartell <ezequiel@thinkmartell.com>
2024-10-25 03:19:20 +0000

    gh actions work

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,7 @@
-output "prod_lb_domain" {
-  value = aws_lb.prod.dns_name
+output "task_definition_arn_backend" {
+  value = aws_ecs_task_definition.prod_backend_web.arn
+}
+
+output "task_definition_arn_frontend" {
+  value = aws_ecs_task_definition.prod_frontend_web.arn
 }
